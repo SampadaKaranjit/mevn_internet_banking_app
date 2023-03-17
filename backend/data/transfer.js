c8c14bd17edd0f48f0ee1cd16cef99ec08fa0560
Sampada Karanjit <sampada.karanjit@gmail.com>
2023-03-17 12:54:25 +0100

    add sample data for seeder

---
+++ backend/data/transfer.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    receiverName: "Jane Doe",
+    receiverIBAN: "DE12345678910",
+    amount: 1500,
+    transactionFee: 0,
+    transactionType: "Standard",
+    note: "Personal Transfer",
+  },
+  {
+    receiverName: "John Doe",
+    receiverIBAN: "DE1112131415",
+    amount: 1600,
+    transactionFee: 0,
+    transactionType: "Standard",
+    note: "Personal Transfer Return With Interests",
+  },
+];
